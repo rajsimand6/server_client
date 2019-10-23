011b4907b3eacbca137b4be188efcc9a819a9174
rajsimand6 <simandalahirajesh681@gmail.com>
2019-10-23 21:25:46 +0200

    add coloured text ability

--- server.rb
+++ server.rb
@@ -12,3 +12,14 @@ puts "Ready to accept new clients on port #{portNum}"
 $numUsers = 0
 
 clients = []
+
+# Choose colors from ColorizedString that are easy to see on dark background
+$colours = Array.new(8)
+$colours[0] = ColorizedString.colors[4] # green
+$colours[1] = ColorizedString.colors[5] # light green
+$colours[2] = ColorizedString.colors[7] # light yellow
+$colours[3] = ColorizedString.colors[8] # blue
+$colours[4] = ColorizedString.colors[9] # light blue
+$colours[5] = ColorizedString.colors[10] # magenta
+$colours[6] = ColorizedString.colors[11] # light magenta
+$colours[7] = ColorizedString.colors[12] # cyan
