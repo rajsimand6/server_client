a8c35ca5d61f86d31b06cd6a640bc9bb653b4b25
rajsimand6 <simandalahirajesh681@gmail.com>
2019-10-23 22:41:51 +0200

    add instant messaging ability

--- client.rb
+++ client.rb
@@ -9,3 +9,49 @@ userName = ARGV.shift
 portNum = 2000
 socket = TCPSocket.new('localhost', portNum)
 socket.puts "#{userName}"
+
+def user_typing(socket, name)
+	loop do
+		sleep 1 # wait 1 second so that text appears properly
+		print "(#{name}) >> ".white
+		message = gets.chomp
+		if message == "exit" # allow user to exit the client script
+			print "Exiting...\n".red
+			socket.puts message
+			exit(0)
+		end
+		socket.puts message
+		
+	end
+end
+
+def receive_from_server(socket, name)
+	while line = socket.gets
+		# Another user joins or leaves
+		if (line.include? "joined!" or line.include? "left!") and !line.include? name
+			print "\n"
+			puts line
+			print "(#{name}) >> ".white
+		# The user joins or leaves
+		elsif (line.include? "joined!" or line.include? "left!") and line.include? name
+			puts line
+		# Another user sends a message
+		elsif !line.include? name
+			print "\n"
+			puts line
+			print "(#{name}) >> ".white
+		# The user sends a message
+		else
+			puts line
+		end
+	end
+end
+
+
+user_typing_thread = Thread.new { user_typing(socket, userName) }
+receive_from_server_thread = Thread.new { receive_from_server(socket, userName) }
+
+user_typing_thread.join
+receive_from_server_thread.join
+
+socket.close
--- server.rb
+++ server.rb
@@ -23,3 +23,51 @@ $colours[4] = ColorizedString.colors[9] # light blue
 $colours[5] = ColorizedString.colors[10] # magenta
 $colours[6] = ColorizedString.colors[11] # light magenta
 $colours[7] = ColorizedString.colors[12] # cyan
+
+def client_message(clients, client, userColour) # runs each time a user enters the chat
+	client_name = client.gets.chomp
+	client.puts "Hello #{client_name}! Users connected: #{clients.count}".yellow
+	transmit_message(clients, "#{client_name} joined!", userColour)
+	
+	while line = client.gets
+		message_from_client = line.chomp
+
+		if message_from_client == "exit"
+			break
+		end
+
+		transmit_message(clients, "#{client_name}: #{message_from_client}", userColour)
+	end
+	
+	client.close
+	clients.delete(client)
+	transmit_message(clients, "#{client_name} left!", userColour)
+end
+	
+
+def transmit_message(clients, text, userColour)
+	i = 0
+	clients.each_with_index do |client|
+		client.puts text.colorize(userColour)
+	end
+end
+
+j = 0
+loop do # runs each time a user enters the chat
+	client = server.accept
+	clients << client
+	$numUsers = $numUsers + 1
+	
+	# assign a colour for each new user
+	if j > $colours.length
+		userColour = $colours[-1]
+	else
+		userColour = $colours[j]
+	end
+	
+	Thread.new {
+		client_message(clients, client, userColour)
+	}
+	
+	j = j+1
+end
